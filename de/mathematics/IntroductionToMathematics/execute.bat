945792dd9864fbedf04b8d51fe25feeadaf243bc
kesper <stephan.kesper@gmail.com>
2014-01-28 15:16:41 +0100

    Primes

--- de/mathematics/IntroductionToMathematics/execute.bat
+++ de/mathematics/IntroductionToMathematics/execute.bat
@@ -1,5 +1,18 @@
 @echo off
 
+erase *.aux
+erase *.log
+erase algebra\*.aux
+erase algebra\*.log
+erase linear-algebra\*.aux
+erase linear-algebra\*.log
+erase analysis\*.aux
+erase analysis\*.log
+erase arithmetic\*.aux
+erase arithmetic\*.log
+erase geometry\*.aux
+erase geometry\*.log
+
 pdflatex IntroductionToMathematics.tex
 pdflatex IntroductionToMathematics.tex
 
